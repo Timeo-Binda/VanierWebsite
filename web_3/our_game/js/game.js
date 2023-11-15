19a6148b021e647838206fd2af6a886be7cba776
Timeo-Binda <timeo.binda@edu.univ-fcomte.fr>
2023-11-15 14:30:36 -0500

    wizzrd peut tirer et tuer. une balle un mort.

--- web_3/our_game/js/game.js
+++ web_3/our_game/js/game.js
@@ -14,6 +14,14 @@ bgImage.onload = function () {
     bgReady = true;
 };
 
+// Variables pour suivre la position de la souris
+var mouseX = 0;
+var mouseY = 0;
+
+addEventListener("mousemove", function (e) {
+    mouseX = e.clientX - canvas.getBoundingClientRect().left;
+    mouseY = e.clientY - canvas.getBoundingClientRect().top;
+});
 // Win frame image
 var winReady = false;
 var winImage = new Image();
@@ -30,6 +38,22 @@ playerImage.onload = function () {
     playerReady = true;
 };
 
+// Bullet image
+var bulletReady = false;
+var bulletImage = new Image();
+bulletImage.src = "images/weapon.png"; // Remplacez "images/bullet.png" par le chemin de votre image de balle
+bulletImage.onload = function () {
+    bulletReady = true;
+};
+
+bulletImage.onerror = function () {
+    console.error("Erreur de chargement de l'image de la balle.");
+};
+bulletImage.onload = function () {
+    bulletReady = true;
+};
+
+
 // Enemy image
 var enemyReady = false;
 var enemyImage = new Image();
@@ -41,20 +65,35 @@ enemyImage.onload = function () {
 // Create global game objects
 var player = {
     speed: 5, // movement in pixels per tick
-    width: 32,
-    height: 32,
-    x: (canvas.width - 32) / 2,
-    y: (canvas.height - 32) / 2,
+    width: 64,
+    height: 64,
+    x: (canvas.width - 64) / 2,
+    y: (canvas.height - 64) / 2,
     lives: 5
 };
+
+var bullet = {
+    speed: 30, // vitesse de la balle en pixels par tick
+    width: 32,
+    height: 32,
+    x: 0,
+    y: 0,
+    visible: false // la balle n'est pas visible au début
+};
+
 
 
 // Create enemies
 var enemies = [
-    { width: 32, height: 32, x: (Math.random() * (canvas.width - 32)), y: (Math.random() * (canvas.height - 32)), enemySpeed: 0.5, cooldown: 0 },
-    { width: 32, height: 32, x: (Math.random() * (canvas.width - 32)), y: (Math.random() * (canvas.height - 32)), enemySpeed: 0.5, cooldown: 0 },
+    { width: 32, height: 32, x: (Math.random() * (canvas.width - 32)), y: (Math.random() * (canvas.height - 32)), enemySpeed: 0.2, cooldown: 0 },
+    { width: 32, height: 32, x: (Math.random() * (canvas.width - 32)), y: (Math.random() * (canvas.height - 32)), enemySpeed: 0.8, cooldown: 0 },
     { width: 32, height: 32, x: (Math.random() * (canvas.width - 32)), y: (Math.random() * (canvas.height - 32)), enemySpeed: 0.5, cooldown: 0 }
 ];
+
+var gameOver = function () {
+    alert("Game Over!");
+    location.reload(); // Rechargez la page pour recommencer le jeu
+};
 
 
 // View variables
@@ -65,20 +104,56 @@ var viewY = 0;
 addEventListener("keydown", function (e) {
     // Keystrokes
     switch (e.key) {
-        case "ArrowUp":
+        case "z":
             player.y -= player.speed;
             break;
-        case "ArrowDown":
+        case "s":
             player.y += player.speed;
             break;
-        case "ArrowLeft":
+        case "q":
             player.x -= player.speed;
             break;
-        case "ArrowRight":
+        case "d":
             player.x += player.speed;
             break;
     }
 }, false);
+
+
+
+
+
+
+
+
+
+// Modifiez le code de tir de la balle
+addEventListener("keydown", function (e) {
+    // Keystrokes
+    switch (e.key) {
+        case " ": // Barre d'espace pour tirer
+            if (!bullet.visible) {
+                // Si la balle n'est pas déjà visible, la positionner sur le joueur et la rendre visible
+                bullet.x = player.x + (player.width - bullet.width) / 2;
+                bullet.y = player.y + (player.height - bullet.height) / 2;
+
+                // Calculez la direction de la souris par rapport au joueur
+                var angle = Math.atan2(mouseY - bullet.y, mouseX - bullet.x);
+
+                // Normalisez la direction pour une précision accrue
+                var normalizedX = Math.cos(angle);
+                var normalizedY = Math.sin(angle);
+
+                // Définissez la vitesse de la balle en fonction de la direction de la souris
+                bullet.speedX = normalizedX * bullet.speed;
+                bullet.speedY = normalizedY * bullet.speed;
+
+                bullet.visible = true;
+            }
+            break;
+    }
+}, false);
+
 
 // Set initial state
 var init = function () {
@@ -91,8 +166,37 @@ var init = function () {
 
 
 
-// Facteur de zoom (augmentez cette valeur pour un zoom plus important)
-var zoomFactor = 8; // Par exemple, pour un zoom 8 fois plus grand
+var updateBullet = function () {
+    if (bullet.visible) {
+        // Mettez à jour la position de la balle en fonction de sa vitesse
+        bullet.x += bullet.speedX;
+        bullet.y += bullet.speedY;
+
+        // Vérifiez les collisions de la balle avec les ennemis
+        for (var i in enemies) {
+            if (checkCollision(bullet, enemies[i])) {
+                // Gérez la collision avec un ennemi ici
+                // Par exemple, supprimez l'ennemi ou réduisez sa vie
+                enemies.splice(i, 1);
+                bullet.visible = false; // Cachez la balle après la collision
+                // Vous pouvez ajouter d'autres actions ici en fonction de vos besoins
+            }
+        }
+
+        // Cacher la balle si elle sort de l'écran ou a parcouru une certaine distance
+        var maxDistance = 500; // Changez la distance maximale selon vos besoins
+        if (bullet.x > canvas.width || bullet.y > canvas.height || bullet.x < 0 || bullet.y < 0 || distance(player, bullet) > maxDistance) {
+            bullet.visible = false;
+        }
+    }
+};
+
+
+var distance = function (obj1, obj2) {
+    var dx = obj1.x - obj2.x;
+    var dy = obj1.y - obj2.y;
+    return Math.sqrt(dx * dx + dy * dy);
+};
 
 
 // The main game loop
@@ -118,13 +222,24 @@ var main = function () {
         }
 
 
-for (var i in enemies) {
-    var angle = Math.atan2(player.y - enemies[i].y, player.x - enemies[i].x);
-    enemies[i].x += Math.cos(angle) * enemies[i].enemySpeed; // utilisez enemySpeed au lieu de player.speed
-    enemies[i].y += Math.sin(angle) * enemies[i].enemySpeed;
-}
 
-for (var i in enemies) {
+
+    for (var i in enemies) {
+        // Vérifiez la collision entre les ennemis et les obstacles
+        for (var j in obstacles) {
+            if (checkCollision(enemies[i], obstacles[j])) {
+                // Si une collision est détectée, ajustez la position de l'ennemi en conséquence
+                // Par exemple, si l'ennemi touche un obstacle, réduisez sa vitesse à 0
+                enemies[i].enemySpeed = 0;
+            }
+        }
+
+        if (enemies[i].enemySpeed !== 0) {
+            var angle = Math.atan2(player.y - enemies[i].y, player.x - enemies[i].x);
+            enemies[i].x += Math.cos(angle) * enemies[i].enemySpeed;
+            enemies[i].y += Math.sin(angle) * enemies[i].enemySpeed;
+        }
+        
         // Vérifiez la collision et gérez les vies du joueur uniquement si le cooldown de l'ennemi est épuisé
         if (enemies[i].cooldown === 0 && checkCollision(player, enemies[i])) {
             // Gérez la collision avec un ennemi ici
@@ -141,8 +256,17 @@ for (var i in enemies) {
             enemies[i].cooldown--;
         }
     
+    
 }
 
+
+if (player.lives <= 0) {
+        // Mettez en œuvre les actions de "Game Over" ici
+        // Par exemple, réinitialisez le jeu ou affichez un écran de fin de partie.
+        gameOver()// Réinitialiser le jeu
+    }
+
+    updateBullet();
 
 
 
@@ -171,6 +295,9 @@ if (enemyReady) {
         ctx.drawImage(enemyImage, enemies[i].x - viewX, enemies[i].y - viewY);
     }
 }
+    if (bulletReady && bullet.visible) {
+        ctx.drawImage(bulletImage, bullet.x - viewX, bullet.y - viewY, bullet.width, bullet.height);
+    }
 
     if (obstacles.length > 0) {
         ctx.fillStyle = "rgb(100, 100, 100)"; // Gris foncé
@@ -204,9 +331,10 @@ var checkWin = function () {
 };
 
 var obstacles = [
-    { width: 64, height: 64, x: 100, y: 100 }, // Premier obstacle
-    { width: 64, height: 64, x: 200, y: 200 }, // Deuxième obstacle
-    // Ajoutez plus d'obstacles selon vos besoins
+    { width: 64, height: 256, x: 100, y: 100 }, // Premier obstacle
+    { width: 64, height: 64, x: 200, y: 200 },
+    { width: 64, height: 64, x: 200, y: 600 }, // Deuxième obstacle
+    
 ];
 
 
--- web_5/portfolio/js/js_home.js
+++ web_5/portfolio/js/js_home.js
@@ -2,32 +2,32 @@ let constrain = 20;
 let maxPerspective = 100; // La valeur maximale de la perspective
 let ex1Layer = document.getElementById("ex1-layer");
 
-function transforms(x, y, el) {
-  let box = el.getBoundingClientRect();
-  let calcX = -(y - box.y - (box.height / 2)) / constrain;
-  let calcY = (x - box.x - (box.width / 2)) / constrain;
+if (ex1Layer) {
+  function transforms(x, y, el) {
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - (box.height / 2)) / constrain;
+    let calcY = (x - box.x - (box.width / 2)) / constrain;
 
-  // Limiter la perspective pour éviter une inclinaison excessive sur l'axe Y
-  let perspective = Math.min(maxPerspective, Math.abs(calcY) * 10);
+    // Limiter la perspective pour éviter une inclinaison excessive sur l'axe Y
+    let perspective = Math.min(maxPerspective, Math.abs(calcY) * 10);
 
-  return "perspective(" + perspective + "px) "
-    + "   rotateX("+ calcX +"deg) "
-    + "   rotateY("+ calcY +"deg) ";
-};
+    return "perspective(" + perspective + "px) "
+      + "   rotateX("+ calcX +"deg) "
+      + "   rotateY("+ calcY +"deg) ";
+  }
 
-function transformElement(el, xyEl) {
-  el.style.transform = transforms.apply(null, xyEl);
+  function transformElement(el, xyEl) {
+    el.style.transform = transforms.apply(null, xyEl);
+  }
+
+  document.onmousemove = function(e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([ex1Layer]);
+
+    window.requestAnimationFrame(function(){
+      transformElement(ex1Layer, position);
+    });
+  };
+} else {
+  console.log("L'élément ex1-layer n'a pas été trouvé.");
 }
-
-document.onmousemove = function(e) {
-  let xy = [e.clientX, e.clientY];
-  let position = xy.concat([ex1Layer]);
-
-  window.requestAnimationFrame(function(){
-    transformElement(ex1Layer, position);
-  });
-
-  } else {
-    console.log("L'élément ex1-layer n'a pas été trouvé.");
-
-};
